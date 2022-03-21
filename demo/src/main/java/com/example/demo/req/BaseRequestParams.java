8392c3309e084ecabc4c7b87b949e3162351b7dd
oever <910246864@qq.com>
2022-03-21 18:11:35 +0800

    提交更改

---
+++ demo/src/main/java/com/example/demo/req/BaseRequestParams.java
@@ -0,0 +1,15 @@
+package com.example.demo.req;
+
+import lombok.Data;
+
+/**
+ * @author gjk
+ * <p> 2022-03-21 </p>
+ * <p> 基础请求类 </p>
+ */
+@Data
+public class BaseRequestParams {
+
+    private Integer version;
+
+}
