b7b086245180942e15a2e18257b1fbec4786e7f2
oever <910246864@qq.com>
2022-03-21 17:36:43 +0800

    提交项目

---
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gjk
+ * <p> 2022-03-21 </p>
+ * <p>  </p>
+ */
+@RestController
+public class TestController {
+
+
+
+}
